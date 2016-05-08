be1e8c020e908422039bc5d9ce63f89e8ea3b19c
neo1218 <neo1218@yeah.net>
2016-05-08 21:45:41 +0800

    fix sure api

--- xueer/api_1_0/sure.py
+++ xueer/api_1_0/sure.py
@@ -13,24 +13,11 @@ from . import api
 from flask import request, jsonify
 
 
-@api.route('/check_email/', methods=["GET", "POST"])
-def check_email():
+@api.route('/sure/', methods=["GET", "POST"])
+def sure():
     if request.method == "POST":
         email = request.get_json().get('email')
         user = User.query.filter_by(email=email).first()
-        if user:
-            return jsonify({
-              "user": "true"
-            })
-        else:
-            return jsonify({"user": "false"})
-
-
-@api.route('/check_username/', methods=['GET', 'POST'])
-def check_username():
-    if request.method == "POST":
-        username= request.get_json().get('username')
-        user = User.query.filter_by(username=username).first()
         if user:
             return jsonify({
               "user": "true"
