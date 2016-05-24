9ebd934e39fe1d77f06a3ce85c748146d09dce07
neo1218 <neo1218@yeah.net>
2016-05-25 05:55:32 +0800

    fix some bug and add course.html

--- xueer/hello/views.py
+++ xueer/hello/views.py
@@ -6,6 +6,7 @@ from flask import render_template,request
 from jinja2 import Environment
 from . import hello
 from xueer.models import Banner, Courses
+from xueer.models import CourseCategories
 
 
 def is_mobie():
@@ -23,14 +24,16 @@ def index():
         return render_template("hello/mobile/index.html")
     else:
         banner = Banner.query.all()
-        gong_top_list = sorted(Courses.query.filter_by(category_id=1), lambda x: x.count, reverse=True)
-        zhuan_top_list = sorted(Courses.query.filter_by(category_id=3), lambda x: x.count, reverse=True)
-        tong_top_list = sorted(Courses.query.filter_by(category_id=2), lambda x: x.count, reverse=True)
-        su_top_list = sorted(Courses.query.filter_by(category_id=4), lambda x: x.count, reverse=True)
-        return render_template("hello/desktop/pages/index.html", banner=banner,
-                gong_top_list=gong_top_list, su_top_list=su_top_list
-                zhuan_top_list=zhuan_top_list, tong_top_list=tong_top_list
-                )
+        gong_list = Courses.query.filter_by(category_id=1).all()
+        gong_top_list = sorted(gong_list, key=lambda x: x.count, reverse=True)[:5]
+        zhuan_top_list = sorted(Courses.query.filter_by(category_id=3).all(), key=lambda x: x.count, reverse=True)[:5]
+        tong_top_list = sorted(Courses.query.filter_by(category_id=2).all(), key=lambda x:x.count, reverse=True)[:5]
+        su_top_list = sorted(Courses.query.filter_by(category_id=4).all(), key=lambda x:x.count, reverse=True)[:5]
+        return render_template(
+            "hello/desktop/pages/index.html", banner=banner,
+            gong_top_list=gong_top_list, su_top_list=su_top_list,
+            zhuan_top_list=zhuan_top_list, tong_top_list=tong_top_list
+        )
 
 # placehold 路由
 @hello.route('/search-result/', methods=['GET'])
@@ -48,7 +51,19 @@ def course(id):
     if is_mobie():
         return render_template("hello/mobile/index.html")
     else:
-        return render_template("hello/desktop/pages/index.html")
+        info = Courses.query.get_or_404(id)
+        category = {
+            1: "公",
+            2: "通",
+            3: "专",
+            4: "素"
+        }.get(info.category_id)
+        hot_comments = []
+        new_comments = []
+        return render_template(
+            "hello/desktop/pages/courses.html",
+            info = info, category = category
+        )
 
 @hello.route('/tip/<int:id>/', methods=['GET'])
 def tip(id):
@@ -98,7 +113,7 @@ def register():
 @hello.route('/category/')
 def category():
     """ category """
-    if is mobile():
+    if is_mobile():
         return render_template("hello/mobile/index.html")
     else:
         return render_template("hello/desktop/pages/category.html")
