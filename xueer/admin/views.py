b894b6022b9520b977355db80d2f4d6f8b9aa434
neo1218 <neo1218@yeah.net>
2016-06-05 12:17:45 +0800

    fix admin site permission

--- xueer/admin/views.py
+++ xueer/admin/views.py
@@ -13,10 +13,11 @@
 
 from . import admin
 from flask import render_template
-from flask_login import login_required
+from flask_login import login_required, current_user
+from xueer.decorators import admin_login
 
 
 @admin.route('/')
-@login_required
+@admin_login
 def index():
     return render_template("admin/index.html")
