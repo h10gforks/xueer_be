dbaacc7e4de1b3ec7f77e802dc67c35a82f0e212
neo1218 <neo1218@yeah.net>
2016-06-06 19:36:29 +0800

    fix flask0.10 2 0.11 issue

---
+++ tests/api_tests/test_api_authentication.py
@@ -0,0 +1,6 @@
+# coding: utf-8
+
+from xueer.api_1_0.authentication import get_token
+
+
+def test_authentication():
--- xueer/auth/forms.py
+++ xueer/auth/forms.py
@@ -6,7 +6,7 @@
     : 同步登录模块表单
 
 """
-from flask.ext.wtf import Form
+from flask_wtf import Form
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Length, Email, Regexp, EqualTo
 from wtforms import ValidationError
