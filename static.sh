b638491bc4dd8f1bbe67503639b8150edf909540
neo1218 <neo1218@yeah.net>
2016-06-08 18:33:50 +0800

    rm static shell file

--- static.sh
+++
@@ -1,9 +0,0 @@
-################# 学而静态文件部署脚本 ##################
-# xueer_be xueer_static
-echo " (ง •_•)ง -> deploy static file..."
-cp -r ../xueer_static/static/ ./xueer
-
-echo " (ง •_•)ง -> deploy templates file..."
-cp -r ../xueer_static/templates/ ./xueer
-
-################# 学而静态文件部署脚本 ##################
