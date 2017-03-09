5bed08adea16a2fbf619c32399ccd308d8adccd7
neo1218 <neo1218@yeah.net>
2017-03-09 12:58:26 +0800

    merge

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 ###############  学而自动部署脚本 #################
-redis-server /etc/redis/redis6380.conf
-redis-server /etc/redis/redis6385.conf
-redis-server
+redis-server /etc/redis/redis6380.conf&
+redis-server /etc/redis/redis6385.conf&
+echo "xueer redis restarted!"
 supervisorctl stop xueer
 python manage.py db upgrade
 supervisorctl start xueer
--- xueer/__init__.py
+++ xueer/__init__.py
@@ -30,9 +30,9 @@ login_manager = LoginManager()
 moment = Moment()
 
 # Initial redis for keywords store
-rds = redis.StrictRedis(host='localhost', port=6380, db=0, password=os.getenv("REDIS6380PASS"))
+rds = redis.StrictRedis(host='localhost', port=6380, db=0, password="foobared6380")
 # Initial redis for LRU/memory cache
-lru = redis.StrictRedis(host='localhost', port=6385, db=1, password=os.getenv("REDIS6385PASS"))
+lru = redis.StrictRedis(host='localhost', port=6385, db=1, password="foobared6385")
 
 from . import models
 
