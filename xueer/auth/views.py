6402d3ce04932dbbe6c1fd16929ec4399aabd7eb
kasheemlew <417879751@qq.com>
2017-12-17 17:51:12 +0800

    revise admin login status

--- xueer/auth/views.py
+++ xueer/auth/views.py
@@ -8,7 +8,7 @@
 """
 
 from . import auth
-from flask import render_template, url_for, redirect
+from flask import render_template, url_for, redirect, request
 from flask_login import login_user, logout_user, login_required
 from xueer.models import User
 from .forms import LoginForm
@@ -17,13 +17,12 @@ from .forms import LoginForm
 @auth.route('/login/', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
-    if form.validate_on_submit():
+    if request.method == 'POST':
         user = User.query.filter_by(email=form.email.data).first()
         if user is not None and user.verify_password(form.password.data) and \
                 user.is_administrator():
             login_user(user)
-            # return redirect(url_for("admin.index"))
-            return render_template("admin/index.html")
+            return redirect(url_for('admin.index'))
     return render_template('auth/login.html', form=form)
 
 
