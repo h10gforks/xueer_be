7d3eaebe44db29c3881717b481f7536e73d8cb46
Andrewpqc <m13636038496@163.com>
2018-05-23 23:54:02 +0800

    fix:an implicit bug about using 'eval' :bug:

--- xueer/api_1_0/courses.py
+++ xueer/api_1_0/courses.py
@@ -135,7 +135,9 @@ def put_course(id):
     course = Courses.query.get_or_404(id)
     if request.method == "PUT":
         # eval is evil:( but
-        data_dict = eval(request.data)
+        #warning: do not use eval,use `json.dumps()` instead!!!
+        # data_dict = eval(request.data)
+        data_dict=json.dumps(request.date)
         course.name = data_dict.get('name', course.name)
         course.teacher = data_dict.get('teacher', course.teacher)
         course.category_id = data_dict.get('category_id', course.category_id)
