72346949022054439bede0709a7e46cbff8f7627
Andrewpqc <m13636038496@163.com>
2018-06-07 21:27:15 +0800

    feat: rollback

--- xueer/api_1_0/promotion.py
+++ xueer/api_1_0/promotion.py
@@ -23,11 +23,11 @@ def promotion_link():
     return jsonify({'private_promotion_link': current_user.generate_private_promotion_link()}), 200
 
 
-@api.route("/promotion/top/", methods=['GET'])
-def get_promotion_top():
-    top_list = sorted(User.query.filter(User.recommend_count>0).all(), key=lambda x: x.recommend_count, reverse=True)
-    return json.dumps(
-        [user.to_json2() for user in top_list],
-        ensure_ascii=False,
-        indent=1
-    ), 200
+# @api.route("/promotion/top/", methods=['GET'])
+# def get_promotion_top():
+#     top_list = sorted(User.query.filter(User.recommend_count>0).all(), key=lambda x: x.recommend_count, reverse=True)
+#     return json.dumps(
+#         [user.to_json2() for user in top_list],
+#         ensure_ascii=False,
+#         indent=1
+#     ), 200
